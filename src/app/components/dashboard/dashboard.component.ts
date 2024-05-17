00ea890eb1ccc7a6178157a8672740bc3797ca90
Matteo Cappella <matteo.cappella@leitha.eu>
2024-05-17 20:02:23 +0200

    Update description

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  static FIRST_YEAR_WORK = 2018;
 
+  linkedinIcon = faLinkedin;
+  githubIcon = faGithub;
+
+  experienceYears = 0;
+
+  ngOnInit(): void {
+    const currentYear = (new Date()).getFullYear();
+    this.experienceYears = currentYear - DashboardComponent.FIRST_YEAR_WORK;
+  }
 }
